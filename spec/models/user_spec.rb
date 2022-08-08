c8602e333ad3da728c124fa16b79a30907f1c3f8
keisacct <107605705+keisacct@users.noreply.github.com>
2022-08-08 10:13:51 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+  # ユーザー新規登録についてのテストコードを記述します
+    it 'nicknameが空では登録できない' do
+    # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'emailが空では登録できない' do
+    # emailが空では登録できないテストコードを記述します
+    end
+  end
 end
